9d310e3205c1463e17c6cf5d8506df070fbda523
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-14 23:23:09 +0200

    add typing list for union

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ type-annotated function sum_mixed_list"""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """sum mixed list"""
     sum = 0
     for i in mxd_lst:
