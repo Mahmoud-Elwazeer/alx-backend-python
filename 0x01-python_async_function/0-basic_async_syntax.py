4c694817c0495b80aa66d05596543eed7245549b
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-16 00:16:48 +0200

    add type-annotated

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """random delay between 0 and max_delay"""
     val = max_delay * random.random()
 
