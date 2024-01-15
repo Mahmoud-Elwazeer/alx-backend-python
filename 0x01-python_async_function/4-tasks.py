7755129cb0769d4c1d5905c6f209b96c2eec4c54
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-01-16 01:30:32 +0200

    no thing new

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""import modules"""
+import asyncio
+from typing import List, Sequence
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """the list of all the delays (float values). T"""
+    out: List[float] = []
+
+    for i in range(n):
+        out.append(await wait_random(max_delay))
+
+    out = sorted(out)
+    return out
