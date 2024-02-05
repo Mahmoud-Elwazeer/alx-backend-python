b8557153756d9a2395ff9109dfa8c29e32b69910
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-05 23:33:32 +0200

    mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """import libraries"""
 import unittest
+from unittest.mock import MagicMock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """class using usnittest to test methdods"""
+    """class using usnittest to test access_nested_map func"""
     @parameterized.expand([
         ({"a": 1}, ("a",)),
         ({"a": {"b": 2}}, ("a",)),
@@ -14,7 +15,7 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path):
         """test accesdd nested map"""
-        self.assertEqual(access_nested_map({"a": 1}, ("a",)), 1)
+        self.assertEqual(utils.access_nested_map({"a": 1}, ("a",)), 1)
 
     @parameterized.expand([
         ({}, ("a",)),
@@ -23,7 +24,19 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """test accesdd nested map exception"""
         with self.assertRaises(KeyError):
-            access_nested_map({}, ("a",))
+            utils.access_nested_map({}, ("a",))
+
+
+class TestGetJson(unittest.TestCase):
+    """class using usnittest to test get_json func"""
+    @patch('requests.get')
+    def test_get_json(self, mock):
+        """test get json func"""
+        mock.return_value = {"payload": True}
+
+        result = utils.get_json("http://example.com")
+
+        self.assertEqual(result, {"payload": True})
 
 
 if __name__ == '__main__':
