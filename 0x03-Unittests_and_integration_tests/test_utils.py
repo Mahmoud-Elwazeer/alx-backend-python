a905bcb7d208bd00fb3ce96c2c623db86c6625bb
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-05 22:41:22 +0200

    testtunit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,13 +7,14 @@ import parameterized
 class TestAccessNestedMap(unittest.TestCase):
     """class using usnittest to test methdods"""
     @parameterized.expand([
-        nested_map={"a": 1}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a", "b")
+        ({"a": 1}, ("a",)),
+        ({"a": {"b": 2}}, ("a",)),
+        ({"a": {"b": 2}}, ("a", "b"))
     ])
     def test_access_nested_map(self, nested_map, path):
         """test accesdd nested map"""
-        self.assertEqual(({"a": 1}, "a"), "a")
+        self.assertEqual(({"a": 1}, ("a",)), "a")
+
 
 if __name__ == '__main__':
     unittest.main()
