9e1f9a7bbdeda72889df9c1d5711411b4e1fb76d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-06 22:11:34 +0200

    assign log to time

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,13 +2,19 @@
 
 import sqlite3
 import functools
+from datetime import datetime
 
-#### decorator to lof SQL queries
+def log_message(message):
+    # Get the current timestamp
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Print the log message with the timestamp
+    print(f"{timestamp} - Executing query: {message}")
 
 def log_queries(func):
     def wrapper(*args, **kwargs):
         query = kwargs.get('query', args[0] if args else None)
-        print(query)
+        log_message(query)
         func(*args, **kwargs)
 
     return wrapper
