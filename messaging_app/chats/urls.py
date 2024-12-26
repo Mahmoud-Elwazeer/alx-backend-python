7d0d73a83691b2076f5318722c6ea9e6fe448ddb
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-26 17:51:18 +0200

    #

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,13 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+# Create a router and register the viewsets
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
-urlpatterns = router.urls
+# Define the urlpatterns using the router
+auth_api_urlpatterns = [
+    path('', include(router.urls)), 
+]
